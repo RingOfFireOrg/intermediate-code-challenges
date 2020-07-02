a69e688bea7a37d40aede0db5918f2a1f73f4f8f
jasonw52603 <minecrafter10k@gmail.com>
2020-07-02 18:20:34 -0400

    created challenge

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -9,10 +9,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 
 public class Robot extends TimedRobot {
@@ -21,37 +18,12 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
-  Joystick rightStick;
-  Joystick leftStick;
-
-  JoystickButton shooterButton;
-  JoystickButton transferButton;
-  JoystickButton intakeButton;
-
-  WPI_TalonSRX shooterMotor;
-  WPI_TalonSRX transferMotor;
-  WPI_TalonSRX intakeMotor;
-  WPI_TalonSRX rightDriveMotor;
-  WPI_TalonSRX leftDriveMotor;
 
   @Override
   public void robotInit() {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
-
-    rightStick = new Joystick(1);
-    leftStick = new Joystick(2);
-
-    shooterButton = new JoystickButton(rightStick, 1);
-    transferButton = new JoystickButton(rightStick, 2);
-    intakeButton = new JoystickButton(rightStick, 3);
-
-    shooterMotor = new WPI_TalonSRX(0);
-    transferMotor = new WPI_TalonSRX(1);
-    intakeMotor = new WPI_TalonSRX(2);
-    rightDriveMotor = new WPI_TalonSRX(3);
-    leftDriveMotor = new WPI_TalonSRX(4);
   }
 
   @Override
@@ -80,20 +52,7 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-    if (shooterButton.get()){
-      shooterMotor.set(0.95);
-    }
 
-    if (transferButton.get()){
-      transferMotor.set(0.4);
-    }
-
-    if (intakeButton.get()){
-      intakeMotor.set(0.8);
-    }
-
-    rightDriveMotor.set(rightStick.getY());
-    leftDriveMotor.set(leftStick.getY());
   }
 
   @Override
